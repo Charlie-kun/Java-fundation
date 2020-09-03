68b0034201fb3c87531be29f82f6e142b8954faf
Charlie-kun <56343000+Charlie-kun@users.noreply.github.com>
2020-09-03 16:53:02 +0900

    Create Double.isNan.java

---
+++ Day Study/Double.isNan.java
@@ -0,0 +1,18 @@
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    double x=5.0;
+    double y=0.0;
+
+    double z=x%y;
+
+    if(Double.isNaN(z))
+    {
+      System.out.println("Can't divide to 0.0");
+    }else{
+      double result =z+10;
+      System.out.println("result : " +result);
+    }
+  }
+}
